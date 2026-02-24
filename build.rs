cd088d06f976390a3c91f216df5b842576b856fc
Oleksii Moisieienko <oleksiimoiseenko@gmail.com>
2026-02-24 14:06:57 +0300

    build.rs

--- build.rs
+++ build.rs
@@ -3,20 +3,16 @@ use std::path::PathBuf;
 
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-
     let header_path = PathBuf::from(&crate_dir)
         .join("include")
         .join("airgap.h");
-
     std::fs::create_dir_all(header_path.parent().unwrap()).unwrap();
-
     cbindgen::Builder::new()
         .with_crate(&crate_dir)
         .with_config(cbindgen::Config::from_file("cbindgen.toml").unwrap())
         .generate()
         .expect("Unable to generate bindings")
         .write_to_file(&header_path);
-
     println!("cargo:rerun-if-changed=src/");
     println!("cargo:rerun-if-changed=cbindgen.toml");
 }
